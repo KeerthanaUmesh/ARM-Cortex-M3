2374b18a09456961755c20a99a07381f17c735ff
Keerthana Umesh <147648970+KeerthanaUmesh@users.noreply.github.com>
2024-08-13 01:41:37 +0530

    Create ring_counter.c

---
+++ LED/ring_counter.c
@@ -0,0 +1,35 @@
+#include<LPC17xx.H>
+
+void delay(unsigned long int x);
+
+int main()
+{
+	unsigned int c;
+	c=1;
+SystemInit (); // MUST
+LPC_SC->PCONP=0X00008000;
+LPC_GPIO0->FIOMASK1=0XF0;
+LPC_GPIO0->FIODIR1=0X0F;
+while(1)    //superloop
+ {
+
+LPC_GPIO0->FIOSET1=c; 
+delay(0X1000);
+LPC_GPIO0->FIOCLR1=c;
+delay(0X1000);
+	 c=c<<1;
+	 if(c>16)
+		 c=1;
+
+ }
+
+}
+void delay(unsigned long int x)
+{
+unsigned int i;
+unsigned int j;
+for(i=0;i<x;i++)
+{
+for(j=0;j<114;j++);
+}
+}
